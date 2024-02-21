8fcd1e99917ef01608761694e076e09b20fdbe82
stuartcat <118708775+stuartcat@users.noreply.github.com>
2024-02-21 11:45:14 -0800

    Create run.js

---
+++ run.js
@@ -0,0 +1,8 @@
+var i = 1
+window.addEventListener("load", function (e) {
+    if (i == 1) {
+        var x = document.write('<iframe src="https://www.google.com" width="100%" height="100%" class="s"></iframe><style>body {height: 100%;overflow: hidden;margin: 0;}</style>')
+        
+        i = i + 1
+    }
+})
